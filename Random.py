69f343e3913f5c6cfad9c93e6376ba28ca9ef5b8
Hakaabi <87300208+Hakaabi@users.noreply.github.com>
2021-07-14 11:01:17 +0300

    I uploaded the program

---
+++ Random.py
@@ -0,0 +1,37 @@
+# Haya Calculator:
+def addition(x, y: object):
+    return x + y
+
+
+def subtraction(x, y: object):
+    return x - y
+
+
+def multiplication(x, y: object):
+    return x * y
+
+
+def division(x, y: object):
+    return x / y
+
+
+print("Select operation.")
+print("1.+")
+print("2.-")
+print("3.×")
+print("4.÷")
+
+Select = input("Enter numbers(1/2/3/4):")
+Number_1 = int(input("First Number: "))
+Number_2 = int(input("Second Number: "))
+
+if Select == "1":
+    print(Number_1 + Number_2)
+elif Select == "2":
+    print(Number_1 - Number_2)
+elif Select == "3":
+    print(Number_1 * Number_2)
+elif Select == "4":
+    print(Number_1 / Number_2)
+else:
+    print("Error")
